bcd11907f58ff61b5d86162d02623b17c3ff7cb5
blerdline <blerdline@gmail.com>
2023-08-06 21:50:33 -0500

    Init app commit

---
+++ arva_field_management_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArvaFieldManagementAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'arva_field_management_app'
